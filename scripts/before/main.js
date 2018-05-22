68e87ea77ec34ba3d54ea0f49962c8d9ef80b3df
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2018-05-22 11:40:46 +0200

    panorama header

--- scripts/before/main.js
+++ scripts/before/main.js
@@ -240,7 +240,96 @@ let current = 0,
 setInterval(function() {
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.opacity = 0;
+    slides[i].style.width 
   }
   current = (current != slides.length - 1) ? current + 1 : 0;
   slides[current].style.opacity = 1;
 }, 2000);
+
+/* panorama canvas*/
+
+let img = new Image();
+
+// User Variables - customize these to change the image being scrolled, its
+// direction, and the speed.
+
+img.src = '../img/bkg/sumava-panorama.jpg';
+let CanvasXSize = 2700;
+let CanvasYSize = 720;
+let speed = 60; // lower is faster
+let scale = 1.05;
+let y = -4.5; // vertical offset
+
+// Main program
+
+let dx = 0.75;
+let imgW;
+let imgH;
+let x = 0;
+let clearX;
+let clearY;
+let ctx;
+
+img.onload = function() {
+    imgW = img.width * scale;
+    imgH = img.height * scale;
+    
+    if (imgW > CanvasXSize) {
+        // image larger than canvas
+        x = CanvasXSize - imgW;
+    }
+    if (imgW > CanvasXSize) {
+        // image width larger than canvas
+        clearX = imgW;
+    } else {
+        clearX = CanvasXSize;
+    }
+    if (imgH > CanvasYSize) {
+        // image height larger than canvas
+        clearY = imgH;
+    } else {
+        clearY = CanvasYSize;
+    }
+    
+    // get canvas context
+    ctx = document.getElementById('canvas').getContext('2d');
+ 
+    // set refresh rate
+    return setInterval(draw, speed);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, clearX, clearY); // clear the canvas
+    
+    // if image is <= Canvas Size
+    if (imgW <= CanvasXSize) {
+        // reset, start from beginning
+        if (x > CanvasXSize) {
+            x = -imgW + x;
+        }
+        // draw additional image1
+        if (x > 0) {
+            ctx.drawImage(img, -imgW + x, y, imgW, imgH);
+        }
+        // draw additional image2
+        if (x - imgW > 0) {
+            ctx.drawImage(img, -imgW * 2 + x, y, imgW, imgH);
+        }
+    }
+
+    // image is > Canvas Size
+    else {
+        // reset, start from beginning
+        if (x > (CanvasXSize)) {
+            x = CanvasXSize - imgW;
+        }
+        // draw aditional image
+        if (x > (CanvasXSize-imgW)) {
+            ctx.drawImage(img, x - imgW + 1, y, imgW, imgH);
+        }
+    }
+    // draw image
+    ctx.drawImage(img, x, y,imgW, imgH);
+    // amount to move
+    x += dx;
+}
