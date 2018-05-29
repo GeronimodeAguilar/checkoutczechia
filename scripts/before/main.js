09eb9f30daa726ac5ae36bd18de6fe29fa43cade
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2018-05-29 22:48:35 +0200

    accordion + krumlov

--- scripts/before/main.js
+++ scripts/before/main.js
@@ -168,6 +168,22 @@ document.querySelector('.down-arrow').onclick = function () {
   window.scrollTo(0,600); 
 }
 
+/*javascript accordion*/
+
+var accItem = document.getElementsByClassName('accordionItem');
+var accHD = document.getElementsByClassName('accordionItemHeading');
+for (i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
+}
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'accordionItem close-accord';
+    }
+    if (itemClass == 'accordionItem close-accord') {
+        this.parentNode.className = 'accordionItem open-acccord';
+    }
+}
 
 // Show an element
 let show = function (elem) {
