e3bb4f5a4c29b2debffdc6de305408470f25fca9
GeronimodeAguilar <35854084+GeronimodeAguilar@users.noreply.github.com>
2018-06-11 11:29:55 +0200

    tilted text background slider + fixed auto grid slider

--- scripts/before/ternary.js
+++ scripts/before/ternary.js
@@ -1,12 +1,14 @@
-          let slideCount = document.querySelectorAll('.background-slider .slide-item').length;
-        let slideWidth = document.querySelectorAll('.slider-outer')[0].clientWidth;
-        let slideHeight = document.querySelectorAll(".slider-outer")[0].clientHeight;
-      
+        let slideCount = document.querySelectorAll('.background-slide').length;
+        let slideWidth = document.querySelector('.background-slider').clientWidth;
+        let slideHeight = document.querySelector(".background-slider").clientHeight;
+        let tiltedSlideText = document.querySelectorAll('.tilted-text');
         let sliderUlWidth = slideCount * slideWidth;
         document.querySelectorAll('.background-slider')[0].style.cssText = "width:" + sliderUlWidth + "px";
-      
         for (let i = 0; i < slideCount; i++) {
-          document.querySelectorAll('.slide-item')[i].style.cssText = "width:" + slideWidth + "px;height:" + slideHeight + "px";
+          let backgroundWidth = document.querySelector('.background-slider').clientWidth;
+          document.querySelectorAll('.background-slide')[i].style.cssText = "width:" + slideWidth + "px;height:" + slideHeight + "px";
+          document.querySelectorAll('.tilted-text')[i].style.cssText = `font-size: ${(slideWidth / 7)}px; color: hsla(211, 76%, 28%,1);
+          background-color: hsla(211, 76%, 28%,0.01);`;
         }
       
         setInterval(function() {
@@ -20,7 +22,6 @@
               let transformSize = slideWidth * slideNum;
               document.querySelectorAll('.background-slider')[0].style.cssText = 
                 "width:" + sliderUlWidth + "px; -webkit-transition:all 800ms ease; -webkit-transform:translate3d(-" + transformSize + "px, 0px, 0px);-moz-transition:all 800ms ease; -moz-transform:translate3d(-" + transformSize + "px, 0px, 0px);-o-transition:all 800ms ease; -o-transform:translate3d(-" + transformSize + "px, 0px, 0px);transition:all 800ms ease; transform:translate3d(-" + transformSize + "px, 0px, 0px)";
-      
             } else {
               counter = 1;
               document.querySelectorAll('.background-slider')[0].style.cssText = "width:" + sliderUlWidth + "px;-webkit-transition:all 800ms ease; -webkit-transform:translate3d(0px, 0px, 0px);-moz-transition:all 800ms ease; -moz-transform:translate3d(0px, 0px, 0px);-o-transition:all 800ms ease; -o-transform:translate3d(0px, 0px, 0px);transition:all 800ms ease; transform:translate3d(0px, 0px, 0px)";
